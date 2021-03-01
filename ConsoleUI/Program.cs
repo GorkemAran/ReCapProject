34ca4d0ac3b15cdfd4aaff8a0667a845bae3e717
Görkem Aran <GrkemAran@hotmail.com>
2021-03-01 15:21:38 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+
+            foreach (var item in carManager.GetAll())
+            {
+                Console.WriteLine(item.CarId +" " + item.BrandId);
+                
+                
+            }
+        }
+    }
+}
